c3d9aa5604c60d74225058c903f24b34a2430fd4
yukito-takeuchi <iyggf66974@yahoo.ne.jp>
2025-10-25 11:39:54 +0900

    deploy

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,6 +6,10 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 pool.on("connect", () => {
